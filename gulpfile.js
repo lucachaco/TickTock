6590b59c8f79724a09afe73bf52212cebd8513a6
lucachaco <luiscarlosch@gmail.com>
2015-06-19 19:41:32 -0600

    Add gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    minifyCss = require('gulp-minify-css');
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify');
 
 gulp.task('minify-css', function () {
     return gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css', 'app/css/app.css'])
         .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(concat('min.css'))
         .pipe(gulp.dest('app/css/'));
+});
+
+
+gulp.task('uglify-js', function () {
+    return gulp.src(['bower_components/angular/angular.js', 'app/js/app.js', 'app/js/controllers.js'])
+        .pipe(uglify())
+        .pipe(concat('min.js'))
+        .pipe(gulp.dest('app/js/'));
+});
+
+
+gulp.task('default', ['minify-css', 'uglify-js'], function () {
 });
